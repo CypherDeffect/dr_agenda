3cca3a4094a2db9cc166d32820236724acca22c0
Leonardo Manoel Oliveira Machado <341484@unoesc.lan>
2024-08-28 20:55:48 -0300

    Forms cadastro usuario

---
+++ dr_agenda/cadastro-usuario.php
@@ -0,0 +1,15 @@
+<?php
+include 'conexao.php';
+
+
+/* 
+
+LINKS
+
+https://www.devmedia.com.br/criando-um-sistema-de-cadastro-e-login-com-php-e-mysql/37213
+https://www.w3schools.com/html/html_forms.asp
+
+
+*/
+
+?>
--- dr_agenda/conexao.php
+++ dr_agenda/conexao.php
@@ -4,8 +4,13 @@ $bancodedados = "dragenda";
 $usuario = "root";
 $senha = "root";
 
+
+// Cria conexão 
 $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+// Valida se conexão ocorreu com sucesso
 if ($mysqli->connect_errno) {
     echo "Falha ao conectar ao banco $bancodedados! (". $mysqli->connect_error.") " . $mysqli->connect_error;
 
 }
+?>
